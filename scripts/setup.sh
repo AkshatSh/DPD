820a2d882176f864e5a167d84ffd30b5170e7d3c
Akshat Shrivastava <akshatshrivastava@hotmail.com>
2019-04-11 11:09:31 -0700

    updated setup script

---
+++ scripts/setup.sh
@@ -0,0 +1,16 @@
+#!bin/bash
+
+# create virtual env
+DIR_ENV="./env"
+
+if [ ! -d "$DIR_ENV" ]; then
+  echo "Creating virtual environment ..."
+  python3 -m venv env
+  source env/bin/activate
+  pip install -r deps/requirements.txt
+  deactivate
+else
+  echo "Using existing virtual environment .. "
+fi
+
+echo "Finished setup..."
