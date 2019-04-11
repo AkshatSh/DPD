d091ce40be4f87b1effb2e9c4c99f826562aae69
Akshat Shrivastava <akshatshrivastava@hotmail.com>
2019-04-11 11:22:38 -0700

    updated setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,11 +8,19 @@ if [ ! -d "$DIR_ENV" ]; then
   python3 -m venv env
   source env/bin/activate
   pip install -r deps/requirements.txt
-  python -m spacy download en
-  python scripts/setup_nltk.py
   deactivate
 else
   echo "Using existing virtual environment .. "
 fi
 
-echo "Finished setup..."
+# update spacy corpi
+echo "Downloading spaCy en..."
+python -m spacy download en
+echo "Finished spaCy en"
+
+# setup NLTK corpi
+echo "Downloading NLTK Corpi..."
+python scripts/setup_nltk.py
+echo "Finished NLTK Corpi"
+
+echo "Finished setup"
