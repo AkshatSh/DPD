6fd515c223be3c1fbff2f12435ab4c01838d731b
Akshat Shrivastava <akshatshrivastava@hotmail.com>
2019-04-20 00:16:18 -0700

    added CADEC constants

--- dpd/constants.py
+++ dpd/constants.py
@@ -13,3 +13,9 @@ CONLL2003_DIR = os.path.join(os.path.dirname(__file__), '..', 'data', 'conll2003
 CONLL2003_TRAIN = os.path.join(CONLL2003_DIR, 'train.txt')
 CONLL2003_TEST = os.path.join(CONLL2003_DIR, 'test.txt')
 CONLL2003_VALID = os.path.join(CONLL2003_DIR, 'valid.txt')
+
+
+# CADEC Dataset Paths
+CADEC_DIR = os.path.join(os.path.dirname(__file__), '..', 'data', 'cadec')
+CADEC_TRAIN = os.path.join(CADEC_DIR, 'cadec_train_post_conll.txt')
+CADEC_VALID = os.path.join(CADEC_DIR, 'cadec_valid_post_conll.txt')
