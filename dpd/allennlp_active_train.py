582243ba6d37655bf0b983fad41bb4b5edf42cb0
Akshat Shrivastava <akshatshrivastava@hotmail.com>
2019-04-22 17:12:31 -0700

    more skeleton code for our allennlp al pipeline

--- dpd/allennlp_active_train.py
+++ dpd/allennlp_active_train.py
@@ -80,6 +80,8 @@ def train(
     )
     metrics = trainer.train()
 
+    # TODO(akshats): Log some metrics
+
     return model
 
 
@@ -94,7 +96,50 @@ def active_train(
     num_epochs: int,
     device: str,
 ) -> Model:
-    pass
+
+    oracle_samples = [1, 5, 10, 25, 50, 100, 200, 400, 400]
+    for i, sample_size in enumerate(iteration_samples):
+        # select new points from distribution
+        # TODO: implement heuristics
+        distribution = heuritic.evaluate(model, unlabeled_dataset, device)
+        new_points = []
+        sample_size = min(sample_size, len(distribution) - 1)
+        new_points = torch.multinomial(distribution, sample_size)
+        new_points = new_points[:sample_size]
+
+
+        # use new points to augment train_dataset
+        # remove points from unlabaled corpus
+        query = [
+            unlabeled_dataset.data[ind]
+            for ind in new_points
+        ]
+
+        labeled_indexes.extend(
+            ind for (ind, _) in query
+        )
+
+        # adds a weight
+        outputs = [oracle.get_label(q) + (1.0,) for q in query]
+
+        # move unlabeled points to labeled points
+        [unlabeled_dataset.remove(q) for q in query]
+
+        # TODO: build weak_set
+        # TODO: train model
+
+        # gather some metrics
+        f1_data, acc = utils.compute_f1_dataloader(model, test_data_loader, tag_vocab, device=device)
+        f1_avg_valid = utils.compute_avg_f1(f1_data)
+
+        # TODO(akshats): implement logger
+        # log valid metics
+        logger.scalar_summary("active valid f1", f1_avg_valid, len(train_data))
+        logger.scalar_summary("active valid accuracy", acc, len(train_data))
+        utils.log_metrics(logger, f1_data, "active valid", len(train_data))
+
+        # TODO(akshats): use logger
+        print(f'Finished experiment on training set size: {len(train_data)}')
 
 def get_args() -> argparse.ArgumentParser:
     '''
