ebc86e5df01b658da679b15f0172ca834c2d4e1a
Akshat Shrivastava <akshatshrivastava@hotmail.com>
2019-04-26 19:49:42 -0700

    added fine tune experiment script

--- experiments/batch_experiments/dictionary_functions/generate_experiments_script.py
+++ experiments/batch_experiments/dictionary_functions/generate_experiments_script.py
@@ -8,6 +8,7 @@ from typing import (
 
 import os
 import sys
+import argparse
 
 NUM_TRIALS = 3
 WEAK_WEIGHT = [0.01]
@@ -20,7 +21,7 @@ def generate_experiment_commands() -> List[str]:
         for weak_weight in WEAK_WEIGHT:
             for weak_function in WEAK_FUNCTIONS:
                 model_name = f'{weak_weight}/{weak_function}/trial_{trial}'
-                command = f'python dpd/allennlp_active_train.py --weak_function {weak_function} --num_epochs {NUM_EPOCHS} --weak_weight {weak_weight} --model_name {model_name} --use_weak --cuda'
+                command = f'python dpd/allennlp_active_train.py --weak_function {weak_function} --num_epochs {NUM_EPOCHS} --weak_weight {weak_weight} --model_name {model_name} --use_weak --cuda --use_weak_fine_tune'
                 commands.append(command)
     return commands
 
@@ -28,8 +29,9 @@ def get_bash_header() -> List[str]:
     return ['#!/bin/bash','set -e'] 
 
 def main():
+    parser = argparse.ArgumentParser(description='Generates a bash script for running many experiments')
     bash_commands = get_bash_header() + generate_experiment_commands()
-    bash_script = os.path.join(os.path.dirname(__file__), 'weak_active_model_experiments.sh')
+    bash_script = os.path.join(os.path.dirname(__file__), 'weak_fine_tune_active_model_experiments.sh')
     with open(bash_script, 'w') as f:
         for line in bash_commands:
             f.writelines(f'{line}\n')
