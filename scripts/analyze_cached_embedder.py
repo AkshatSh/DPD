8da201a88b38243e1fa473ec3e8c9ae756d83652
Akshat Shrivastava <akshatshrivastava@hotmail.com>
2019-04-28 21:43:15 -0700

    added analysis script for cached embedder

---
+++ scripts/analyze_cached_embedder.py
@@ -0,0 +1,30 @@
+from typing import (
+    List,
+    Tuple,
+    Dict,
+    Optional,
+)
+
+import os
+import sys
+
+import torch
+import numpy as np
+import allennlp
+
+from dpd.utils import SaveFile, H5SaveFile
+from dpd.models.embedder import CachedTextFieldEmbedder
+from dpd.constants import (
+    CADEC_NER_ELMo,
+)
+
+def main():
+    save_file = H5SaveFile(save_file=CADEC_NER_ELMo)
+    ce = CachedTextFieldEmbedder(text_field_embedder=None)
+    ce.setup_cache(dataset_id=0)
+    ce.setup_cache(dataset_id=1)
+
+    print(ce)
+
+if __name__ == "__main__":
+    main()
