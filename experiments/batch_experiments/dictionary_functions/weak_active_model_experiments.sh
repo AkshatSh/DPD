5b2e3597e30ae8280a4db6681c61327aaef3e89e
Akshat Shrivastava <akshatshrivastava@hotmail.com>
2019-04-30 21:32:28 -0700

    added top k sampling to experiments

--- experiments/batch_experiments/dictionary_functions/weak_active_model_experiments.sh
+++ experiments/batch_experiments/dictionary_functions/weak_active_model_experiments.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -e
-python dpd/allennlp_active_train.py --weak_function linear --num_epochs 5 --weak_weight 0.01 --model_name cached/weighted/0.01/linear/trial_0 --use_weak --cuda --cache
-python dpd/allennlp_active_train.py --weak_function knn --num_epochs 5 --weak_weight 0.01 --model_name cached/weighted/0.01/knn/trial_0 --use_weak --cuda --cache
-python dpd/allennlp_active_train.py --weak_function keyword --num_epochs 5 --weak_weight 0.01 --model_name cached/weighted/0.01/keyword/trial_0 --use_weak --cuda --cache
-python dpd/allennlp_active_train.py --weak_function linear --num_epochs 5 --weak_weight 0.01 --model_name cached/weighted/0.01/linear/trial_1 --use_weak --cuda --cache
-python dpd/allennlp_active_train.py --weak_function knn --num_epochs 5 --weak_weight 0.01 --model_name cached/weighted/0.01/knn/trial_1 --use_weak --cuda --cache
-python dpd/allennlp_active_train.py --weak_function keyword --num_epochs 5 --weak_weight 0.01 --model_name cached/weighted/0.01/keyword/trial_1 --use_weak --cuda --cache
-python dpd/allennlp_active_train.py --weak_function linear --num_epochs 5 --weak_weight 0.01 --model_name cached/weighted/0.01/linear/trial_2 --use_weak --cuda --cache
-python dpd/allennlp_active_train.py --weak_function knn --num_epochs 5 --weak_weight 0.01 --model_name cached/weighted/0.01/knn/trial_2 --use_weak --cuda --cache
-python dpd/allennlp_active_train.py --weak_function keyword --num_epochs 5 --weak_weight 0.01 --model_name cached/weighted/0.01/keyword/trial_2 --use_weak --cuda --cache
-python dpd/allennlp_active_train.py --weak_function linear --num_epochs 5 --weak_weight 0.01 --model_name cached/weighted/0.01/linear/trial_3 --use_weak --cuda --cache
-python dpd/allennlp_active_train.py --weak_function knn --num_epochs 5 --weak_weight 0.01 --model_name cached/weighted/0.01/knn/trial_3 --use_weak --cuda --cache
-python dpd/allennlp_active_train.py --weak_function keyword --num_epochs 5 --weak_weight 0.01 --model_name cached/weighted/0.01/keyword/trial_3 --use_weak --cuda --cache
-python dpd/allennlp_active_train.py --weak_function linear --num_epochs 5 --weak_weight 0.01 --model_name cached/weighted/0.01/linear/trial_4 --use_weak --cuda --cache
-python dpd/allennlp_active_train.py --weak_function knn --num_epochs 5 --weak_weight 0.01 --model_name cached/weighted/0.01/knn/trial_4 --use_weak --cuda --cache
-python dpd/allennlp_active_train.py --weak_function keyword --num_epochs 5 --weak_weight 0.01 --model_name cached/weighted/0.01/keyword/trial_4 --use_weak --cuda --cache
+python dpd/allennlp_active_train.py --weak_function linear --num_epochs 5 --weak_weight 0.01 --model_name cached/weighted/0.01/linear/trial_0 --use_weak --cuda --cache --sample_strategy top_k
+python dpd/allennlp_active_train.py --weak_function knn --num_epochs 5 --weak_weight 0.01 --model_name cached/weighted/0.01/knn/trial_0 --use_weak --cuda --cache --sample_strategy top_k
+python dpd/allennlp_active_train.py --weak_function keyword --num_epochs 5 --weak_weight 0.01 --model_name cached/weighted/0.01/keyword/trial_0 --use_weak --cuda --cache --sample_strategy top_k
+python dpd/allennlp_active_train.py --weak_function linear --num_epochs 5 --weak_weight 0.01 --model_name cached/weighted/0.01/linear/trial_1 --use_weak --cuda --cache --sample_strategy top_k
+python dpd/allennlp_active_train.py --weak_function knn --num_epochs 5 --weak_weight 0.01 --model_name cached/weighted/0.01/knn/trial_1 --use_weak --cuda --cache --sample_strategy top_k
+python dpd/allennlp_active_train.py --weak_function keyword --num_epochs 5 --weak_weight 0.01 --model_name cached/weighted/0.01/keyword/trial_1 --use_weak --cuda --cache --sample_strategy top_k
+python dpd/allennlp_active_train.py --weak_function linear --num_epochs 5 --weak_weight 0.01 --model_name cached/weighted/0.01/linear/trial_2 --use_weak --cuda --cache --sample_strategy top_k
+python dpd/allennlp_active_train.py --weak_function knn --num_epochs 5 --weak_weight 0.01 --model_name cached/weighted/0.01/knn/trial_2 --use_weak --cuda --cache --sample_strategy top_k
+python dpd/allennlp_active_train.py --weak_function keyword --num_epochs 5 --weak_weight 0.01 --model_name cached/weighted/0.01/keyword/trial_2 --use_weak --cuda --cache --sample_strategy top_k
+python dpd/allennlp_active_train.py --weak_function linear --num_epochs 5 --weak_weight 0.01 --model_name cached/weighted/0.01/linear/trial_3 --use_weak --cuda --cache --sample_strategy top_k
+python dpd/allennlp_active_train.py --weak_function knn --num_epochs 5 --weak_weight 0.01 --model_name cached/weighted/0.01/knn/trial_3 --use_weak --cuda --cache --sample_strategy top_k
+python dpd/allennlp_active_train.py --weak_function keyword --num_epochs 5 --weak_weight 0.01 --model_name cached/weighted/0.01/keyword/trial_3 --use_weak --cuda --cache --sample_strategy top_k
+python dpd/allennlp_active_train.py --weak_function linear --num_epochs 5 --weak_weight 0.01 --model_name cached/weighted/0.01/linear/trial_4 --use_weak --cuda --cache --sample_strategy top_k
+python dpd/allennlp_active_train.py --weak_function knn --num_epochs 5 --weak_weight 0.01 --model_name cached/weighted/0.01/knn/trial_4 --use_weak --cuda --cache --sample_strategy top_k
+python dpd/allennlp_active_train.py --weak_function keyword --num_epochs 5 --weak_weight 0.01 --model_name cached/weighted/0.01/keyword/trial_4 --use_weak --cuda --cache --sample_strategy top_k
