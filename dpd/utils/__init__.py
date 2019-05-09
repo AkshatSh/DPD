a0c5571ec44c69091aa860b21e8aa33690732d08
Akshat Shrivastava <akshatshrivastava@hotmail.com>
2019-05-08 19:42:59 -0700

    added PickleFaiss interface in FAISS utils

--- dpd/utils/__init__.py
+++ dpd/utils/__init__.py
@@ -5,3 +5,4 @@ from .utils import *
 from .save_file import SaveFile, H5SaveFile, PickleSaveFile
 from .tensor_utils import TensorList
 from .model_utils import get_cached_embedder, get_all_embedders
+from .faiss_utils import PickleFaiss
